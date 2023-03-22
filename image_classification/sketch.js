ca8db1695bfd3abd2c1ea1c2a631bb8f5ddfff6a
kurianth.pulimoottil <kurianth.pulimoottil@servicenow.com>
2023-03-22 13:07:48 +0530

    upload image and classify it using mobilenet dataset and pretrained model in ml5.js and displayed p5.js

---
+++ image_classification/sketch.js
@@ -0,0 +1,63 @@
+let mobileNetClassifier;
+let puffin;
+let inputElement;
+let userImage;
+
+function setup() {
+  createCanvas(640, 480);
+  background(0);
+
+  inputElement = createFileInput(handleFile);
+  inputElement.position(0, 500);
+  
+  /** pre trained model for image classification */
+  mobileNetClassifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+  /** loading image */
+  puffin = createImg("images/penguin.jpeg", imageReady);
+  puffin.hide();
+
+}
+
+function handleFile(file){
+  print(file);
+
+  if (file.type === 'image') {
+    userImage = createImg(file.data, imageReady);
+    userImage.hide();
+  } else {
+    userImage = null;
+  }
+
+
+  /** prediction */
+  mobileNetClassifier.predict(userImage, predictResult);
+}
+
+function predictResult(error, results){
+  if(error){
+    console.error(error)
+    return ;
+  }
+
+  let max = 0;
+  let mostProbablePrediction;
+  results.forEach(result => {
+    if(result.confidence > max){
+      mostProbablePrediction = result;
+      max = result.confidence
+    }
+  });
+  console.log(results);
+  console.log(mostProbablePrediction.label);
+  console.log(parseFloat(mostProbablePrediction.confidence) * 100);
+}
+
+
+function imageReady(){
+  image(userImage,0,0, width, height);
+}
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
